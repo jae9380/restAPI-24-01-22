f5e37649fbca6282d2ed35270209fb5ddad72d65
Lee,JaeYeol <ljy5314@gmail.com>
2024-01-22 10:28:50 +0900

    setting

---
+++ src/main/java/com/ll/restapi240122/RestApi240122Application.java
@@ -0,0 +1,13 @@
+package com.ll.restapi240122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi240122Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApi240122Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/restapi240122/RestApi240122ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.restapi240122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApi240122ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
