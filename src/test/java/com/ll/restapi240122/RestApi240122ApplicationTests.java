0f46f6be9ee6fd37f585573c874e4ec170598e11
Lee,JaeYeol <ljy5314@gmail.com>
2024-01-22 10:43:20 +0900

    h2접속을 위한 securityConfig 생성

--- src/test/java/com/ll/restapi240122/RestApi240122ApplicationTests.java
+++ src/test/java/com/ll/restapi240122/RestApi240122ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.restapi240122;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestApi240122ApplicationTests {
 
     @Test
